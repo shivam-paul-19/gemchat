77821349379d527ff590e00a9c4ec1571440366d
Shivam Paul <shivampaul2319@gmail.com>
2024-12-19 17:37:23 +0530

    theme added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,18 @@ function App() {
   let [chats, setChats] = useState([]);
   let [initValue, setInitvalue] = useState("");
   let [isLoad, setIsLoad] = useState(false);
+  let [isLight, setIsLight] = useState(true);
+
+  let body = document.querySelector("body");
+  body.style.backgroundColor = (isLight) ? "#f3f4f6" : "#111827";
+
+  let input_style = (isLight)? {
+    backgroundColor: "#030712",
+    color: "white",
+  } : {
+    backgroundColor: "#d1d5db",
+    color: "black",
+  }
 
   const getResponse = async (prompt) => {
     const genAI = new GoogleGenerativeAI(API_KEY);
@@ -38,12 +50,56 @@ function App() {
 
   return (
     <div className="main-area">
-      <h1 style={{
-        position: "absolute",
-        top: "10px",
-        left: "10px",
-        fontSize: "30px"
-      }}>GemChat</h1>
+      <img src="https://i.ibb.co/wCLddFF/gemchat-logo.png" alt="" style={{
+          position: "absolute",
+          top: "10px",
+          left: "10px",
+          height: "50px",
+        }}/>
+      <label className="grid cursor-pointer place-items-center"
+        style={{
+          position: "absolute",
+          top: "10px",
+          right: "10px"
+        }}
+      >
+        <input
+          type="checkbox"
+          onChange={() => {
+            setIsLight(!isLight);
+          }}
+          className="toggle toggle-lg theme-controller bg-base-content col-span-2 col-start-1 row-start-1"
+        />
+        <svg
+          className="stroke-base-100 fill-base-100 col-start-1 row-start-1"
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="14"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <circle cx="12" cy="12" r="5" />
+          <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+        </svg>
+        <svg
+          className="stroke-base-100 fill-base-100 col-start-2 row-start-1"
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="14"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+        </svg>
+      </label>
       <div className="chat">
         <div className="chat-box">
           {chats.length == 0 ? (
@@ -54,16 +110,27 @@ function App() {
             chats.map((el) => {
               return el[1] == 1 ? (
                 <div className="chat chat-start">
-                  <div className="chat-bubble chat-bubble-primary" dangerouslySetInnerHTML={{__html: el[0]}} style={{
-                    backgroundColor: "#374151",
-                    color: "white"
-                  }}></div>
+                  <div
+                    className="chat-bubble chat-bubble-primary"
+                    dangerouslySetInnerHTML={{ __html: el[0] }}
+                    style={{
+                      backgroundColor: "#374151",
+                      color: "white",
+                      overflowX: "scroll",
+                      scrollbarWidth: "none"
+                    }}
+                  ></div>
                 </div>
               ) : (
                 <div className="chat chat-end">
-                  <div className="chat-bubble chat-bubble-error" style={{
-                    backgroundColor: "#d1d5db"
-                  }}>{el[0]}</div>
+                  <div
+                    className="chat-bubble chat-bubble-error"
+                    style={{
+                      backgroundColor: "#d1d5db",
+                    }}
+                  >
+                    {el[0]}
+                  </div>
                 </div>
               );
             })
@@ -76,23 +143,23 @@ function App() {
             value={initValue}
             className="input input-bordered input-primary w-full"
             onChange={changeValue}
-            style={{
-              backgroundColor: "#d1d5db",
-              color: "black"
-            }}
+            style={input_style}
           />
           &nbsp;&nbsp;
           {isLoad ? (
-            <button className="btn btn-square" style={{
-              backgroundColor: "#d1d5db"
-            }}>
+            <button
+              className="btn btn-square"
+              disabled="disabled"
+              style={input_style}
+            >
               <span className="loading loading-dots"></span>
             </button>
           ) : (
-            <button type="submit" className="btn" style={{
-              backgroundColor: "#d1d5db",
-              color: "black"
-            }}>
+            <button
+              type="submit"
+              className="btn"
+              style={input_style}
+            >
               Enter
             </button>
           )}
